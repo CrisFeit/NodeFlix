da5bd990a22ce42516992df7ea8e2fa8ed2e2d7a
crisfeit <crisrfab@gmail.com>
2024-01-27 12:39:13 -0300

    chore: lock dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'precss': {},
+    'postcss-extend': {},
     'postcss-preset-env': {},
-    'cssnano': {}
+    'cssnano': {},
   }
 }
